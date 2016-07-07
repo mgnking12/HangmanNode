a4be512fc6d87bff5bbe8b12385b9ce2a48543a8
Megan King <mgnking12@gmail.com>
2016-07-07 12:19:57 -0500

    words for the game

---
+++ game.js
@@ -0,0 +1,20 @@
+exports.gameWrd = {
+	["crow",
+	"khaleesi",
+	"maester",
+	"meereenese knot",
+	"milk of the poppy",
+	"pyromancer",
+	"raven",
+	"red wedding",
+	"second sons",
+	"the seven",
+	"unsullied",
+	"valyrian",
+	"the wall",
+	"warg",
+	"white walkers",
+	"wildfire",
+	"wilding"
+	]
+}
